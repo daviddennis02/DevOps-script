ea264cbed9c5f7fe8ee12194035c96ad6dbdf721
towolabi <towolabi@seamfix.com>
2021-02-03 14:46:42 +0100

    first update all servicenames on the script

---
+++ service-monitor-alert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE=/var/run/httpd/httpd.pid
+VPSERVICE=/var/run/vpservice.pid
+
+if ! [ -f "$FILE" ]; then
+# confirm Apache is down & start Apache
+mail -s 'Apache is down' ddennis@seamfix.com temeasoba@seamfix.com dunuane@seamfix.com <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and will try restarting in 10s'
+sudo service httpd start 
+fi
+sleep 10s
+if ! [ -f "$FILE" ]; then
+sudo service  httpd start
+fi
+sleep 5s
+
+# confirm AutoTopup service is down and start the service
+if ! [ -f "$VPSERVICE" ]; then
+sudo service vpservice start  
+fi
+
+# Restart Tomcat
+sudo service vasportal start
+sleep 10s
+
+# If all does not go well, alert responsible parties
+if ! [ -f "$FILE $VPSERVICE" ]; then
+mail -s 'Apache is down' ddennis@seamfix.com temeasoba@seamfix.com dunuane@seamfix.com  <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and cannot be restarted'
+fi
