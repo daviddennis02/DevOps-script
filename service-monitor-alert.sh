5e83a2a695eb04d8753ec59c37c98e662e5ba223
daviddennis02 <dennisdavid02@gmail.com>
2021-02-04 11:14:24 +0100

    updated the resolver condition

--- service-monitor-alert.sh
+++ service-monitor-alert.sh
@@ -5,8 +5,9 @@ VPSERVICE=/var/run/vpservice.pid
 
 if ! [ -f "$FILE" ]; then
 # confirm Apache is down & start Apache
-mail -s 'Apache is down' ddennis@seamfix.com temeasoba@seamfix.com dunuane@seamfix.com <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and will try restarting in 10s'
-sudo service httpd start 
+## mail -s 'Apache is down' ddennis@seamfix.com <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and will try restarting in 10s'
+sudo service httpd start
+mail -s 'Apache is down' ddennis@seamfix.com <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and will try restarting in 10s'
 fi
 sleep 10s
 if ! [ -f "$FILE" ]; then
@@ -16,7 +17,7 @@ sleep 5s
 
 # confirm AutoTopup service is down and start the service
 if ! [ -f "$VPSERVICE" ]; then
-sudo service vpservice start  
+sudo service vpservice start
 fi
 
 # Restart Tomcat
@@ -24,6 +25,12 @@ sudo service vasportal start
 sleep 10s
 
 # If all does not go well, alert responsible parties
-if ! [ -f "$FILE $VPSERVICE" ]; then
-mail -s 'Apache is down' ddennis@seamfix.com temeasoba@seamfix.com dunuane@seamfix.com  <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and cannot be restarted'
+if ! [ -f "$FILE" ]; then
+mail -s 'Apache is down' ddennis@seamfix.com <<< 'Apache is down on AUTOTOPUP CLOUD SERVER and cannot be restarted'
 fi
+
+
+## CRONJOB
+#sudo crontab -e
+##paste this
+##  */10 11-21 * * 1-5 ~ddennis/scripts/apache-service-job.sh
